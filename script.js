685a65e89338a27a3498e4f18d8cf0035823f7ff
MajorD13 <tdiscipline@gmail.com>
2024-10-09 15:40:48 +0100

    day 2 update

---
+++ script.js
@@ -0,0 +1,30 @@
+const consentBox = 
+    document.getElementById("consentBox");
+const acceptBtn = 
+    document.querySelector(".consentButton");
+const rejectBtn = 
+    document.querySelector(".rejectButton");
+
+acceptBtn.onclick = () => {
+    document.cookie = "CookieBy=Medway-Norse; max-age="
+        + 60 * 60 * 24;
+    if (document.cookie) {
+        consentBox.classList.add("hide");
+    } else {
+        alert
+            ("Cookie can't be set! Please"+
+              " unblock this site from the cookie"+
+              " setting of your browser.");
+    }
+};
+
+rejectBtn.onclick = () => {
+    alert(
+        "Cookies rejected. Some functionality may be limited.");
+    consentBox.classList.add("hide");
+};
+
+let checkCookie = 
+    document.cookie.indexOf("CookieBy=Medway-Norse");
+checkCookie !== -1 ? consentBox.classList.add("hide") :
+    consentBox.classList.remove("hide");
